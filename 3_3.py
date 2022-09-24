faf27f9bddd4dc5f4c6ee1e1b0b0079b000733a2
RamanSoutsa91 <raman.soutsa@gmail.com>
2022-09-24 11:58:34 +0300

    решение задачи №3 на форматирование строк 3 способами(тренировался с форматом, решил не удалять 4 способ).

---
+++ 3_3.py
@@ -0,0 +1,23 @@
+name = input('Your name: ')
+age = input('Your age: ')
+city = input('Your city: ')
+text = 'Your name ' + name + ', your age ' + str(age) + ', your city ' + city
+print(text)
+
+name = input('Your name: ')
+age = int(input('Your age: '))
+city = input('Your city: ')
+text2 = 'Hello. Your name %s, your age %d, your city %s' % (name, age, city)
+print(text2)
+
+name = input('Your name: ')
+age = int(input('Your age: '))
+city = input('Your city: ')
+text3 = 'Hello. Your name {}. Your age {}. Your city {}.' .format(name, age, city)
+print(text3)
+
+name = input('Your name: ')
+age = int(input('Your age: '))
+city = input('Your city: ')
+text4 = f'Hello. Your name {name}, your age {age}, your city {city}.'
+print(text4)
