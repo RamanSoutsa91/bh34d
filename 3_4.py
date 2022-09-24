82570de5aa73df365c5342148e9d0685b64a3ad0
RamanSoutsa91 <raman.soutsa@gmail.com>
2022-09-24 12:47:42 +0300

    Решение задачи на кол-во положительных и отрицательных чисел

---
+++ 3_4.py
@@ -0,0 +1,6 @@
+# Пользователь вводит 3 числа, сказать сколько из них положительных и сколько отрицательных
+number1 = int(input('enter the number: '))
+number2 = int(input('Enter the number: '))
+number3 = int(input('Enter the number: '))
+print((number1>0) + (number2>0) + (number3>0))
+print((number1<0) + (number2<0) + (number3<0))
